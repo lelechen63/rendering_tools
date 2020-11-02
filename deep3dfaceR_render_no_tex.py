0be69570d9abac21bd0799ae53ae477506fd2d3e
Lele Chen <lelechen@fb.com>
2020-11-02 14:28:34 -0500

    'g'

--- deep3dfaceR_render_no_tex.py
+++ deep3dfaceR_render_no_tex.py
@@ -20,6 +20,16 @@ import json
 res = 224
 from scipy.io import loadmat, savemat
 
+import sys
+import soft_renderer as sr
+import imageio
+from skimage.transform import warp
+from skimage.transform import AffineTransform
+import numpy as np
+import cv2
+
+from scipy.io import loadmat, savemat
+
 def load_obj(obj_file):
     vertices = []
 
@@ -48,40 +58,12 @@ def load_obj(obj_file):
     return (np.array(vertices), np.array(triangles).astype(np.int), np.array(colors))
 
 
-
-def load_obj_without_color(obj_file):
-    vertices = []
-
-    triangles = []
-    colors = []
-
-    with open(obj_file) as infile:
-        for line in infile.read().splitlines():
-            if len(line) > 2 and line[:2] == "v ":
-                ts = line.split()
-                y = float(ts[1])
-                x = float(ts[2])
-                z = float(ts[3])
-                r = float(1)
-                g = float(1)
-                b = float(1)
-                vertices.append([x,y,z])
-                colors.append([r,g,b])
-            elif len(line) > 2 and line[:2] == "f ":
-                ts = line.split()
-                fy = int(ts[1]) - 1
-                fx = int(ts[2]) - 1
-                fz = int(ts[3]) - 1
-                triangles.append([fx,fy,fz])
-    
-    return (np.array(vertices), np.array(triangles).astype(np.int), np.array(colors))
-
 def setup_renderer():    
     renderer = sr.SoftRenderer(
         camera_mode="look", 
-        viewing_scale=2/res, 
-        far=10000, 
-        perspective=False, 
+        far=50.0,
+        near=0.01, 
+        perspective=False,
         image_size=res, 
         camera_direction=[0,0,-1], 
         camera_up=[0,1,0],
@@ -89,9 +71,9 @@ def setup_renderer():
         light_color_ambient=[1,1,1],
         light_intensity_directionals=0.6, 
         light_color_directionals=[1,1,1],
-        light_directions=[-1,0,-0.8]
+        light_directions=[0,0,1]
     )
-    renderer.transform.set_eyes([res/2, res/2, 6000])
+    renderer.transform.set_eyes([0, 0, 10])
     return renderer
 
 def get_np_uint8_image(mesh, renderer):
@@ -103,8 +85,6 @@ def get_np_uint8_image(mesh, renderer):
     image = np.clip(image, 0, 1)
     image = (255*image).astype(np.uint8)
     return image
-
-
 
 def render_single_img( mat_path , obj_path, save_path):
     # overlay = True
@@ -119,21 +99,25 @@ def render_single_img( mat_path , obj_path, save_path):
     renderer = setup_renderer()
         
     # render without texture
-    face_mesh = sr.Mesh(vertices_org, triangles)
+    face_mesh = sr.Mesh(vertices_org, triangles, texture_type="vertex")
 
     image_render = get_np_uint8_image(face_mesh, renderer) # RGBA, (224,224,3), np.uint8
-    print (image_render.max(), image_render.min(),'=====')
-    rgb_frame =  (image_render).astype(int)[:,:,:-1][...,::-1]
-    print (rgb_frame.max(), rgb_frame.min(),'====rgb_frame=')
+    rgb_frame =  (image_render).astype(int)[:,:,:-1]
+
+    rgb_frame = rgb_frame.transpose(1,0,2)[::-1, :, :]
+    
+    # plt.imshow(rgb_frame)
+    # plt.show()
 
     mask_n = rgb_frame.sum(2)
     mask_n[mask_n!=0]=1
     mask = mask_n.reshape(res,res, 1)
     mask = np.repeat(mask, 3, axis = 2)
-    # final_output = cropped_img * (1 - mask) + mask * rgb_frame
+    final_output = cropped_img * (1 - mask) + mask * rgb_frame
 
-    # cropped_img  =cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR)  
-    cv2.imwrite(save_path, image_render)  
+    cropped_img  =cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR)  
+    cv2.imwrite(save_path, final_output)
+
 
 def render_all():
     parser = argparse.ArgumentParser(description='PyTorch Face Reconstruction')
@@ -178,5 +162,6 @@ def render_all():
             save_path =  os.path.join( out_dir  , obj[:-9] +  '._no_tex.png')
             
             render_single_img( mat_path , obj_path, save_path)
+
 
 render_all()
