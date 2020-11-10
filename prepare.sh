1ce77dfa3225db4e438249d21987aae866062672
Lele Chen <lelechen@fb.com>
2020-11-10 17:57:26 -0500

    g

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,5 @@
 cd /u/lchen63/cvpr2021/cvpr2021/rendering_tools
-# conda activate face_alignment
 python df2net_landmark_detect.py --conf ../chen_facestar.json
-python df2net_landmark_detect.py --conf ../gir1_facestar.json
 python df2net_landmark_detect.py --conf ../gir1_iphone.json
 python df2net_landmark_detect.py --conf ../Israel_iphone.json
 python df2net_landmark_detect.py --conf ../jason_facestar.json
