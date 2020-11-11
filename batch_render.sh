0ba08dad8c6bb283dcf4ad5e2ccc354377d7546d
Lele Chen <lelechen@fb.com>
2020-11-10 20:09:44 -0500

    gg

---
+++ batch_render.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+source /u/lchen63/miniconda2/etc/profile.d/conda.sh
+conda init bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# DF2net
+ALG_NAME='DF2net'
+conda activate base
+conda activate pytorch1.1python3
+echo -e "${RED}${ALG_NAME}, using ${CONDA_DEFAULT_ENV}${NC}"
+cd rendering_tools
+python df2net_render_no_tex.py --conf ../chen_facestar.json
+cd ../
+
+# MGCNet
+ALG_NAME='MGCNet'
+conda activate base
+conda activate mgcnet
+echo -e "${RED}${ALG_NAME}, using ${CONDA_DEFAULT_ENV}${NC}"
+cd MGCNet
+python test_image.py --conf ../chen_facestar.json
+cd ../
+
+# Deep3DR
+ALG_NAME='Deep3DR'
+conda activate base
+conda activate pytorch1.1python3
+echo -e "${RED}${ALG_NAME}, using ${CONDA_DEFAULT_ENV}${NC}"
+cd rendering_tools
+python deep3dfaceR_render_no_tex.py --conf ../chen_facestar.json
+cd ../
+
+# PRNet
+ALG_NAME='PRNet'
+conda activate base
+conda activate pytorch1.1python3
+echo -e "${RED}${ALG_NAME}, using ${CONDA_DEFAULT_ENV}${NC}"
+cd rendering_tools
+python prnet_render.py --conf ../chen_facestar.json
+cd ../
+
+# Face_normals
+ALG_NAME='Face_normals'
+conda activate base
+conda activate pytorch1.1python3
+echo -e "${RED}${ALG_NAME}, using ${CONDA_DEFAULT_ENV}${NC}"
+cd rendering_tools
+python face_normals_render.py --conf ../chen_facestar.json
+cd ../
+
+# RingNet
+ALG_NAME='RingNet'
+conda activate base
+conda activate ringnet
+echo -e "${RED}${ALG_NAME}, using ${CONDA_DEFAULT_ENV}${NC}"
+cd RingNet
+python demo.py --conf ../chen_facestar.json
+cd ../
+
+# 3DDFAv2
+ALG_NAME='3DDFAv2'
+conda activate base
+conda activate 3ddfav2
+echo -e "${RED}${ALG_NAME}, using ${CONDA_DEFAULT_ENV}${NC}"
+cd 3DDFA_V2
+python demo.py --conf ../chen_facestar.json
+cd ../
+
+# DONE
+echo -e "${RED}ALL DONE${NC}"
