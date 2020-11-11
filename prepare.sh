bb807918c964b7be8aa1572dc896c99c1e0ae2b5
Lele Chen <lelechen@fb.com>
2020-11-10 19:38:47 -0500

    gg

--- prepare.sh
+++ prepare.sh
@@ -15,7 +15,7 @@
 #generate mat in df2net
 # conda activate df2net
 
-cd /u/lchen63/cvpr2021/cvpr2021/DF2Net
+# cd /u/lchen63/cvpr2021/cvpr2021/DF2Net
 # python demo.py --conf ../chen_facestar.json
 # python demo.py --conf ../gir1_iphone.json
 # python demo.py --conf ../Israel_iphone.json
@@ -24,10 +24,20 @@ cd /u/lchen63/cvpr2021/cvpr2021/DF2Net
 # python demo.py --conf ../shugao_facestar.json
 # python demo.py --conf ../steve_iphone.json
 
-python pointcloud2rawmesh.py --conf ../chen_facestar.json
-python pointcloud2rawmesh.py --conf ../gir1_iphone.json
-python pointcloud2rawmesh.py --conf ../Israel_iphone.json
-python pointcloud2rawmesh.py --conf ../jason_facestar.json
-python pointcloud2rawmesh.py --conf ../kevyn_iphone.json
-python pointcloud2rawmesh.py --conf ../shugao_facestar.json
-python pointcloud2rawmesh.py --conf ../steve_iphone.json
+# python pointcloud2rawmesh.py --conf ../chen_facestar.json
+# python pointcloud2rawmesh.py --conf ../gir1_iphone.json
+# python pointcloud2rawmesh.py --conf ../Israel_iphone.json
+# python pointcloud2rawmesh.py --conf ../jason_facestar.json
+# python pointcloud2rawmesh.py --conf ../kevyn_iphone.json
+# python pointcloud2rawmesh.py --conf ../shugao_facestar.json
+# python pointcloud2rawmesh.py --conf ../steve_iphone.json
+
+
+cd /u/lchen63/cvpr2021/cvpr2021/rendering_tools
+python deep3dfaceR_landmark_detect.py --conf ../chen_facestar.json
+python deep3dfaceR_landmark_detect.py --conf ../gir1_iphone.json
+python deep3dfaceR_landmark_detect.py --conf ../Israel_iphone.json
+python deep3dfaceR_landmark_detect.py --conf ../jason_facestar.json
+python deep3dfaceR_landmark_detect.py --conf ../kevyn_iphone.json
+python deep3dfaceR_landmark_detect.py --conf ../shugao_facestar.json
+python deep3dfaceR_landmark_detect.py --conf ../steve_iphone.json
