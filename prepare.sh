4a51b28c2e4036a0330d94843722c04989369299
Lele Chen <lelechen@fb.com>
2020-11-10 21:03:26 -0500

    gg

--- prepare.sh
+++ prepare.sh
@@ -3,14 +3,14 @@
 
 
 # cd /u/lchen63/cvpr2021/cvpr2021/rendering_tools
-python df2net_landmark_detect.py --conf ../chen_facestar.json
-python df2net_landmark_detect.py --conf ../dani_iphone.json
-python df2net_landmark_detect.py --conf ../gir1_iphone.json
-python df2net_landmark_detect.py --conf ../Israel_iphone.json
-python df2net_landmark_detect.py --conf ../jason_facestar.json
-python df2net_landmark_detect.py --conf ../kevyn_iphone.json
-python df2net_landmark_detect.py --conf ../shugao_facestar.json
-python df2net_landmark_detect.py --conf ../steve_iphone.json
+# python df2net_landmark_detect.py --conf ../chen_facestar.json
+# python df2net_landmark_detect.py --conf ../dani_iphone.json
+# python df2net_landmark_detect.py --conf ../gir1_iphone.json
+# python df2net_landmark_detect.py --conf ../Israel_iphone.json
+# python df2net_landmark_detect.py --conf ../jason_facestar.json
+# python df2net_landmark_detect.py --conf ../kevyn_iphone.json
+# python df2net_landmark_detect.py --conf ../shugao_facestar.json
+# python df2net_landmark_detect.py --conf ../steve_iphone.json
 
 
 #generate mat in df2net
@@ -38,14 +38,14 @@ python df2net_landmark_detect.py --conf ../steve_iphone.json
 
 
 # cd /u/lchen63/cvpr2021/cvpr2021/rendering_tools
-python deep3dfaceR_landmark_detect.py --conf ../dani_iphone.json
-python deep3dfaceR_landmark_detect.py --conf ../chen_facestar.json
-python deep3dfaceR_landmark_detect.py --conf ../gir1_iphone.json
-python deep3dfaceR_landmark_detect.py --conf ../Israel_iphone.json
-python deep3dfaceR_landmark_detect.py --conf ../jason_facestar.json
-python deep3dfaceR_landmark_detect.py --conf ../kevyn_iphone.json
-python deep3dfaceR_landmark_detect.py --conf ../shugao_facestar.json
-python deep3dfaceR_landmark_detect.py --conf ../steve_iphone.json
+# python deep3dfaceR_landmark_detect.py --conf ../dani_iphone.json
+# python deep3dfaceR_landmark_detect.py --conf ../chen_facestar.json
+# python deep3dfaceR_landmark_detect.py --conf ../gir1_iphone.json
+# python deep3dfaceR_landmark_detect.py --conf ../Israel_iphone.json
+# python deep3dfaceR_landmark_detect.py --conf ../jason_facestar.json
+# python deep3dfaceR_landmark_detect.py --conf ../kevyn_iphone.json
+# python deep3dfaceR_landmark_detect.py --conf ../shugao_facestar.json
+# python deep3dfaceR_landmark_detect.py --conf ../steve_iphone.json
 
 
 # deep 3DR
@@ -65,14 +65,14 @@ python deep3dfaceR_landmark_detect.py --conf ../steve_iphone.json
 # # 
 # cd /u/lchen63/cvpr2021/cvpr2021/rendering_tools
 
-python face_normals_landamrk_detect.py --conf ../dani_iphone.json
-python face_normals_landamrk_detect.py --conf ../chen_facestar.json
-python face_normals_landamrk_detect.py --conf ../gir1_iphone.json
-python face_normals_landamrk_detect.py --conf ../Israel_iphone.json
-python face_normals_landamrk_detect.py --conf ../jason_facestar.json
-python face_normals_landamrk_detect.py --conf ../kevyn_iphone.json
-python face_normals_landamrk_detect.py --conf ../shugao_facestar.json
-python face_normals_landamrk_detect.py --conf ../steve_iphone.json
+# python face_normals_landamrk_detect.py --conf ../dani_iphone.json
+# python face_normals_landamrk_detect.py --conf ../chen_facestar.json
+# python face_normals_landamrk_detect.py --conf ../gir1_iphone.json
+# python face_normals_landamrk_detect.py --conf ../Israel_iphone.json
+# python face_normals_landamrk_detect.py --conf ../jason_facestar.json
+# python face_normals_landamrk_detect.py --conf ../kevyn_iphone.json
+# python face_normals_landamrk_detect.py --conf ../shugao_facestar.json
+# python face_normals_landamrk_detect.py --conf ../steve_iphone.json
 
 #conda activate face_normals
 # cd /u/lchen63/cvpr2021/cvpr2021/face_normals
@@ -101,11 +101,11 @@ python face_normals_landamrk_detect.py --conf ../steve_iphone.json
 
 
 # cd /u/lchen63/cvpr2021/cvpr2021/rendering_tools
-python ours_render.py --conf ../dani_iphone.json
-python ours_render.py --conf ../chen_facestar.json
-python ours_render.py --conf ../gir1_iphone.json
-python ours_render.py --conf ../Israel_iphone.json
-python ours_render.py --conf ../jason_facestar.json
-python ours_render.py --conf ../kevyn_iphone.json
-python ours_render.py --conf ../shugao_facestar.json
-python ours_render.py --conf ../steve_iphone.json
+# python ours_render.py --conf ../dani_iphone.json
+# python ours_render.py --conf ../chen_facestar.json
+# python ours_render.py --conf ../gir1_iphone.json
+# python ours_render.py --conf ../Israel_iphone.json
+# python ours_render.py --conf ../jason_facestar.json
+# python ours_render.py --conf ../kevyn_iphone.json
+# python ours_render.py --conf ../shugao_facestar.json
+# python ours_render.py --conf ../steve_iphone.json
