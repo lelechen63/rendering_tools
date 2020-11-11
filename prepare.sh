c3c630c1f970b0795e472dbec9f2af3cd05b354b
Lele Chen <lelechen@fb.com>
2020-11-10 19:47:23 -0500

    gg

--- prepare.sh
+++ prepare.sh
@@ -33,11 +33,20 @@
 # python pointcloud2rawmesh.py --conf ../steve_iphone.json
 
 
-cd /u/lchen63/cvpr2021/cvpr2021/rendering_tools
-python deep3dfaceR_landmark_detect.py --conf ../chen_facestar.json
-python deep3dfaceR_landmark_detect.py --conf ../gir1_iphone.json
-python deep3dfaceR_landmark_detect.py --conf ../Israel_iphone.json
-python deep3dfaceR_landmark_detect.py --conf ../jason_facestar.json
-python deep3dfaceR_landmark_detect.py --conf ../kevyn_iphone.json
-python deep3dfaceR_landmark_detect.py --conf ../shugao_facestar.json
-python deep3dfaceR_landmark_detect.py --conf ../steve_iphone.json
+# cd /u/lchen63/cvpr2021/cvpr2021/rendering_tools
+# python deep3dfaceR_landmark_detect.py --conf ../chen_facestar.json
+# python deep3dfaceR_landmark_detect.py --conf ../gir1_iphone.json
+# python deep3dfaceR_landmark_detect.py --conf ../Israel_iphone.json
+# python deep3dfaceR_landmark_detect.py --conf ../jason_facestar.json
+# python deep3dfaceR_landmark_detect.py --conf ../kevyn_iphone.json
+# python deep3dfaceR_landmark_detect.py --conf ../shugao_facestar.json
+# python deep3dfaceR_landmark_detect.py --conf ../steve_iphone.json
+
+cd /u/lchen63/cvpr2021/cvpr2021/Deep3DFaceReconstruction
+python demo.py --conf ../chen_facestar.json
+python demo.py --conf ../gir1_iphone.json
+python demo.py --conf ../Israel_iphone.json
+python demo.py --conf ../jason_facestar.json
+python demo.py --conf ../kevyn_iphone.json
+python demo.py --conf ../shugao_facestar.json
+python demo.py --conf ../steve_iphone.json
