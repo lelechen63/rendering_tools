27db48241d856c2af719cec7e0c7832b6026e45d
Lele Chen <lelechen@fb.com>
2020-11-10 19:32:39 -0500

    gg

--- prepare.sh
+++ prepare.sh
@@ -16,10 +16,18 @@
 # conda activate df2net
 
 cd /u/lchen63/cvpr2021/cvpr2021/DF2Net
-python demo.py --conf ../chen_facestar.json
-python demo.py --conf ../gir1_iphone.json
-python demo.py --conf ../Israel_iphone.json
-python demo.py --conf ../jason_facestar.json
-python demo.py --conf ../kevyn_iphone.json
-python demo.py --conf ../shugao_facestar.json
-python demo.py --conf ../steve_iphone.json
+# python demo.py --conf ../chen_facestar.json
+# python demo.py --conf ../gir1_iphone.json
+# python demo.py --conf ../Israel_iphone.json
+# python demo.py --conf ../jason_facestar.json
+# python demo.py --conf ../kevyn_iphone.json
+# python demo.py --conf ../shugao_facestar.json
+# python demo.py --conf ../steve_iphone.json
+
+python pointcloud2rawmesh.py --conf ../chen_facestar.json
+python pointcloud2rawmesh.py --conf ../gir1_iphone.json
+python pointcloud2rawmesh.py --conf ../Israel_iphone.json
+python pointcloud2rawmesh.py --conf ../jason_facestar.json
+python pointcloud2rawmesh.py --conf ../kevyn_iphone.json
+python pointcloud2rawmesh.py --conf ../shugao_facestar.json
+python pointcloud2rawmesh.py --conf ../steve_iphone.json
