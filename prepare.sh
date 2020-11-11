857e965a94d50c4b23e7faa086c64f644d41c41a
Lele Chen <lelechen@fb.com>
2020-11-10 19:24:14 -0500

    gg

--- prepare.sh
+++ prepare.sh
@@ -2,23 +2,24 @@
 # conda activate face_alignment
 
 
-# cd /u/lchen63/cvpr2021/cvpr2021/rendering_tools
-# python df2net_landmark_detect.py --conf ../chen_facestar.json
-# python df2net_landmark_detect.py --conf ../gir1_iphone.json
-# python df2net_landmark_detect.py --conf ../Israel_iphone.json
-# python df2net_landmark_detect.py --conf ../jason_facestar.json
-# python df2net_landmark_detect.py --conf ../kevyn_iphone.json
-# python df2net_landmark_detect.py --conf ../shugao_facestar.json
-# python df2net_landmark_detect.py --conf ../steve_iphone.json
+cd /u/lchen63/cvpr2021/cvpr2021/rendering_tools
+python df2net_landmark_detect.py --conf ../chen_facestar.json
+python df2net_landmark_detect.py --conf ../gir1_iphone.json
+python df2net_landmark_detect.py --conf ../Israel_iphone.json
+python df2net_landmark_detect.py --conf ../jason_facestar.json
+python df2net_landmark_detect.py --conf ../kevyn_iphone.json
+python df2net_landmark_detect.py --conf ../shugao_facestar.json
+python df2net_landmark_detect.py --conf ../steve_iphone.json
 
 
 #generate mat in df2net
 # conda activate df2net
-cd /u/lchen63/cvpr2021/cvpr2021/DF2Net
-python demo.py --conf ../chen_facestar.json
-python demo.py --conf ../gir1_iphone.json
-python demo.py --conf ../Israel_iphone.json
-python demo.py --conf ../jason_facestar.json
-python demo.py --conf ../kevyn_iphone.json
-python demo.py --conf ../shugao_facestar.json
-python demo.py --conf ../steve_iphone.json
+
+# cd /u/lchen63/cvpr2021/cvpr2021/DF2Net
+# python demo.py --conf ../chen_facestar.json
+# python demo.py --conf ../gir1_iphone.json
+# python demo.py --conf ../Israel_iphone.json
+# python demo.py --conf ../jason_facestar.json
+# python demo.py --conf ../kevyn_iphone.json
+# python demo.py --conf ../shugao_facestar.json
+# python demo.py --conf ../steve_iphone.json
