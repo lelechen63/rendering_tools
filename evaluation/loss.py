60eacca8dc4d54dfbfe8d8db4b5201890a7efba8
Lele Chen <lelechen@fb.com>
2020-11-11 20:32:07 -0500

    g

--- evaluation/loss.py
+++ evaluation/loss.py
@@ -1,17 +1,41 @@
 import lpips
 import torch
-import pytorch_ssim
 import numpy as np
 import lpips
 
-ssim_loss = pytorch_ssim.SSIM(window_size = 11)
 loss_fn_alex = lpips.LPIPS(net='alex')
 
-# criterion = LPIPS(
-#     net_type='alex',  # choose a network type from ['alex', 'squeeze', 'vgg']
-#     version='0.1',  # Currently, v0.1 is supported
 
-# )
+def ssim(img1, img2, cs_map=False):
+    """Return the Structural Similarity Map corresponding to input images img1 
+    and img2 (images are assumed to be uint8)
+    """
+    img1 = img1.astype(numpy.float64)
+    img2 = img2.astype(numpy.float64)
+    size = 11
+    sigma = 1.5
+    window = gauss.fspecial_gauss(size, sigma)
+    K1 = 0.01
+    K2 = 0.03
+    L = 255 #bitdepth of image
+    C1 = (K1*L)**2
+    C2 = (K2*L)**2
+    mu1 = signal.fftconvolve(window, img1, mode='valid')
+    mu2 = signal.fftconvolve(window, img2, mode='valid')
+    mu1_sq = mu1*mu1
+    mu2_sq = mu2*mu2
+    mu1_mu2 = mu1*mu2
+    sigma1_sq = signal.fftconvolve(window, img1*img1, mode='valid') - mu1_sq
+    sigma2_sq = signal.fftconvolve(window, img2*img2, mode='valid') - mu2_sq
+    sigma12 = signal.fftconvolve(window, img1*img2, mode='valid') - mu1_mu2
+    if cs_map:
+        return (((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*
+                    (sigma1_sq + sigma2_sq + C2)), 
+                (2.0*sigma12 + C2)/(sigma1_sq + sigma2_sq + C2))
+    else:
+        return ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*
+                    (sigma1_sq + sigma2_sq + C2))
+
 def lpips_dis( x, y):
     # x, y size : ( 3, N,N), cv2 readed image, value range (0,255)
     # change it to pytorch tensor, need to be normalized to [-1,1]
@@ -35,9 +59,8 @@ def l2_dis(x, y):
 
 def ssim_dis(x, y):
     # x, y size : ( 3, N,N), cv2 readed image, value range (0,255)
-    x = torch.tensor( x, dtype=torch.float32).cuda().unsequeeze(0)
-    y = torch.tensor( y, dtype=torch.float32).cuda().unsequeeze(0)
-    d = ssim_loss( x, y)
+
+    d = ssim( x, y)
     return d
 
 
