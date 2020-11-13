16610ad96475c24f590b6ecb895fde7872b04e95
Lele Chen <lelechen@fb.com>
2020-11-12 23:11:11 -0500

    g

--- evaluation/csim_dis.py
+++ evaluation/csim_dis.py
@@ -19,6 +19,7 @@ parser.add_argument('--gpu', default=0, type=int, help='gpu id')
 parser.add_argument('--det', default=0, type=int, help='mtcnn option, 1 means using R+O, 0 means detect from begining')
 parser.add_argument('--flip', default=0, type=int, help='whether do lr flip aug')
 parser.add_argument('--threshold', default=1.24, type=float, help='ver dist threshold')
+parser.add_argument('--method', default='ours', type=str, help='ver dist threshold')
 args = parser.parse_args()
 
 # model = face_model.FaceModel(args)
@@ -78,64 +79,121 @@ def read_videos( video_path, crop= False, pos=0):
 
 
 args = parser.parse_args()
-def compute_CSIM(args, path):
+
+def get_list(method = 'deep3dR'):
+    img_list = []
+    if method == 'deep3dR':
+        for root, dirs, files in os.walk('/u/lchen63/cvpr2021/cvpr2021/data/data'):
+            for file in files:
+                if file.endswith(".png"):
+                    img_path = os.path.join(root, file)
+                    if 'deep3dfaceR' in img_path:
+                        if '_no_tex.png' not in img_path:
+                            print (img_path)
+                            img_list.append(img_path)
+    elif method == 'MGCNet':
+        for root, dirs, files in os.walk('/u/lchen63/cvpr2021/cvpr2021/data/data'):
+            for file in files:
+                if file.endswith(".jpg"):
+                    img_path = os.path.join(root, file)
+                    if 'mgcnet' in img_path:
+                        if '_mulPoses' in img_path:
+                            print (img_path)
+                            img_list.append(img_path)
+    elif method =='ours':
+        for root, dirs, files in os.walk('/u/lchen63/cvpr2021/cvpr2021/data/data'):
+            for file in files:
+                if file.endswith(".png"):
+                    img_path = os.path.join(root, file)
+                    if 'ours' in img_path and 'ours_render' not in img_path:
+                        print (img_path)
+                        img_list.append(img_path)
+    elif method =='ours-sh':
+        for root, dirs, files in os.walk('/u/lchen63/cvpr2021/cvpr2021/rendering_tools/evaluation/fid_folder/ours-sh'):
+            for file in files:
+                if file.endswith(".png"):
+                    img_path = os.path.join(root, file)
+                    
+                    img_list.append(img_path)
+    elif method =='jae':
+        for root, dirs, files in os.walk('/u/lchen63/cvpr2021/cvpr2021/rendering_tools/evaluation/fid_folder/jae'):
+            for file in files:
+                if file.endswith(".png"):
+                    img_path = os.path.join(root, file)
+                    
+                    img_list.append(img_path)
+
+    return img_list
+
+def compute_CSIM(args, method ='ours'):
     # path = pathlib.Path(path)
+    f_files = get_list(method)
     model = face_model.FaceModel(args)
-
-    # files = list(path.glob('*real_image*.jpg')) + list(path.glob('*real_image*.png'))
-    # files.sort()
     sims = []
-    # # msssims = []
-    path = pathlib.Path(path)
-    fakefiles = list(path.glob('*fake*.mp4'))
-    # fakefiles = list(path.glob(os.path.join('*','fake.mp4')))
-    # fakefiles = list(path.glob(os.path.join('*', '*', 'test.mp4')))
-    fakefiles.sort()
-    t_files = []
-    f_files = []
-    for v in fakefiles[:5]:
-        for f in read_videos(v, True, 2):
-            f_files.append(f)
-        for f in read_videos(v, True, 1):
-            t_files.append(f)
-
     for i,d in enumerate(tqdm(f_files)):
-            # if i == 10:
-            #     break
+
+        img_p = d
+
+        if method =='ours':
+            result = cv2.imread(img_p)
+            high = result.shape[0]
+            width = int(result.shape[1] /3)
+
+            rec_img = result[:,width : width * 2, :]
+            gt_img = result[:,:width,:]
+        elif method =='MGCNet':
+            result = cv2.imread(img_p)
+            high = result.shape[0]
+            width = int(result.shape[1] /6)
+
+            rec_img = result[:,width : width * 2, :]
+            gt_img = result[:,:width,:]
+        elif method =='deep3dR':
+            result = cv2.imread(img_p)
+            print (result.shape)
+            high = result.shape[0]
+            width = int(result.shape[1] /2)
+
+            rec_img = result[:,width : width * 2, :]
+            gt_img = result[:,:width,:]
+        elif method =='ours-sh':
+            result = cv2.imread(img_p)
+            high = result.shape[0]
+            width = int(result.shape[1] /2)
+            rec_img = result[:,width : width * 2, :]
+            gt_img = result[:,:width,:]
+        elif method =='jae':
+            result = cv2.imread(img_p)
+            high = result.shape[0]
+            width = int(result.shape[1] /3)
+            rec_img = result[:,width : width * 2, :]
+            gt_img = result[:,:width,:]
         try:
-                # real_path = str(d)
-                # fake_path  = real_path.replace("_real_","_synthesized_")
-            f_i = d
-            # cv2.imwrite('/home/cxu-serve/p1/common/WSOL/tmp/false_{}.jpg'.format(i), f_i)
-            f_i = cv2.resize(f_i,(112,112))
-            f_i = model.get_input(f_i)
+            # rec_img = cv2.resize(rec_img,(112,112))
+            print (rec_img.shape, gt_img.shape)
+            f_i = model.get_input(rec_img)
+            print (f_i.shape,'f_i')
             f1 = model.get_feature(f_i)
-            
-            r_i = t_files[i]
-            # cv2.imwrite('/home/cxu-serve/p1/common/WSOL/tmp/true_{}.jpg'.format(i), r_i)
-            r_i = cv2.resize(r_i,(112,112))
-            r_i = model.get_input(r_i)
+        
+            # gt_img = cv2.resize(gt_img,(112,112))
+            print (rec_img.shape, gt_img.shape)
+            r_i = model.get_input(gt_img)
+            print (r_i.shape, 'r_i')
             f2 = model.get_feature(r_i)
             dist = np.sum(np.square(f1-f2))
             sim = np.dot(f1, f2.T)
-            # print(sim)
-            # print (fake_path)
-            # print(sim)
-
             sims.append(sim)
+    
         except:
-            sims.append(0.1)
-        if i == 500:
-            break
+            continue
 
     print (sum(sims) / len(sims))
 
         
 
-path = '/home/cxu-serve/p1/common/ablation/no_atten2' 
 # path = '/home/cxu-serve/p1/common/experiment/nips_lrs/lrs_32_shot'
-# path = args.path
-compute_CSIM(args, path)
+method = args.method
+compute_CSIM(args,method)
 
 # path = pathlib.Path(paths[0]) 0.603
 # realfiles = list(path.glob('*real*.mp4'))
--- evaluation/loss.py
+++ evaluation/loss.py
@@ -1,7 +1,5 @@
-import lpips
 import torch
 import numpy as np
-import lpips
 import sys
 from scipy import signal
 from scipy import ndimage
@@ -10,26 +8,22 @@ import cv2
 from skimage.measure import compare_ssim
 import math
 
-loss_fn_alex = lpips.LPIPS(net='alex')
+# loss_fn_alex = lpips.LPIPS(net='alex')
+# def lpips_dis( x, y):
+#     # x, y size : ( N,N,3), cv2 readed image, value range (0,255)
+#     # change it to pytorch tensor, need to be normalized to [-1,1]
+#     x = torch.tensor( x, dtype=torch.float32)
+#     y = torch.tensor( y, dtype=torch.float32)
+#     x = (x/255.0 - 0.5) *2 
+#     x = torch.clamp(x, min=-1, max=1)
+#     x = x.permute(2,0,1).unsqueeze(0)
 
+#     y = (y/255.0 - 0.5) *2 
+#     y = torch.clamp(y, min=-1, max=1)
+#     y = y.permute(2,0,1).unsqueeze(0)
 
-
-
-def lpips_dis( x, y):
-    # x, y size : ( N,N,3), cv2 readed image, value range (0,255)
-    # change it to pytorch tensor, need to be normalized to [-1,1]
-    x = torch.tensor( x, dtype=torch.float32)
-    y = torch.tensor( y, dtype=torch.float32)
-    x = (x/255.0 - 0.5) *2 
-    x = torch.clamp(x, min=-1, max=1)
-    x = x.permute(2,0,1).unsqueeze(0)
-
-    y = (y/255.0 - 0.5) *2 
-    y = torch.clamp(y, min=-1, max=1)
-    y = y.permute(2,0,1).unsqueeze(0)
-
-    d = loss_fn_alex(x, y).view(-1)
-    return d.detach().numpy()[0]
+#     d = loss_fn_alex(x, y).view(-1)
+#     return d.detach().numpy()[0]
 
 def l2_dis(x, y):
     mse = np.mean( (x - y) ** 2 )
